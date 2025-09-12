b87cafd955f60259a9becf9d785d355bd0f4cc68
Aniket Mahajan <mahajananiket207@gmail.com>
2025-09-12 17:08:49 +0530

    implement compile feature with build step

---
+++ src/core/compiler.ts
@@ -0,0 +1,42 @@
+import { build } from "esbuild";
+import path from "path";
+
+interface CompileOptions {
+  mode?: "run" | "build";
+  outdir?: string;
+}
+export async function compileTs(
+  entryFile: string,
+  option: CompileOptions = { mode: "run" },
+): Promise<string | void> {
+  try {
+    if (option.mode === "build") {
+      await build({
+        entryPoints: [entryFile],
+        bundle: true,
+        platform: "node",
+        format: "cjs",
+        sourcemap: "inline",
+        logLevel: "silent",
+        write: option.mode === "build", // write only in build modes
+        outdir: option.mode === "build" ? (option.outdir ?? "dist") : undefined,
+      });
+      return;
+    } else {
+      const outFile = path.join(process.cwd(), "ghostts_out.js");
+      await build({
+        entryPoints: [entryFile],
+        bundle: true,
+        platform: "node",
+        format: "cjs",
+        sourcemap: "inline",
+        logLevel: "silent",
+        write: true,
+        outfile: outFile,
+      });
+      return outFile;
+    }
+  } catch (error) {
+    throw error;
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,18 @@ import { Command } from "commander";
 
 interface CliOptions {
   watch?: boolean;
+  build?: BuildOptions;
+}
+interface BuildOptions {
+  mode: "build";
+  outdir: string;
 }
 
 const program = new Command();
 
+program.name("ghostts").description("GhostTs -- zero-config Typescript tool");
+
 program
-  .name("ghostts")
-  .description("GhostTs -- zero-config Typescript tool")
   .argument("<file>", "entry .ts file")
   .option("-w, --watch", "watch mode")
   .action(async (file: string, option: CliOptions) => {
@@ -21,9 +26,16 @@ program
         await runFile(file);
       }
     } catch (error) {
-      console.log("Error: ", error.message);
+      console.log("Error: ", error);
       process.exit(1);
     }
   });
+
+program
+  .command("build")
+  .argument("<file>", "entry .ts file")
+  .option("--outdir <dir>", "output directory", "./dist")
+  .action(async (file: string, option: CliOptions) => {});
+//Disk-base compilation for deployment
 
 program.parse();
